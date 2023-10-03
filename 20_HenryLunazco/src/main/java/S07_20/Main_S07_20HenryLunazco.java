f9f64bcc523599100261e93a189d3d94f0ac00b6
Jesus Sulca <jesus.sulca@vallegrande.edu.pe>
2023-10-03 10:24:25 -0500

    ejercicios

---
+++ 20_HenryLunazco/src/main/java/S07_20/Main_S07_20HenryLunazco.java
@@ -0,0 +1,13 @@
+ package S07_20;
+
+public class Main_S07_20HenryLunazco {
+
+    public static void main(String[] args) {
+        Persona2 per = new Persona2();
+        
+        per.apellido="Lunazco";
+        per.nombre="Henry";
+        per.name();
+        System.out.println(per.name());
+    }
+}
---
+++ 20_HenryLunazco/src/main/java/S07_20/Persona1.java
@@ -0,0 +1,12 @@
+package S07_20;
+
+public class Persona1 {
+    //metodos con parametros//
+    
+    //atributos
+    String name;
+    int edad;
+    
+    //metodos
+    
+}
